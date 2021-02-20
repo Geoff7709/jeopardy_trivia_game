ec57d2a582a15a6ebce9f2ad4e91a0ac9a9dc2c6
Geoffrey Zimmerman <geoffzimmerman23@gmail.com>
2021-02-20 11:33:51 -0600

    passport authentication

---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS jeopardy_game_db;
+CREATE DATABASE jeopardy_game_db;
+USE jeopardy_game_db;
--- server.js
+++ server.js
@@ -3,14 +3,19 @@ const express = require("express");
 const session = require("express-session");
 // Requiring passport as we've configured it
 const passport = require("./config/passport");
+const exphbs = require('express-handlebars')
 
 // Setting up port and requiring models for syncing
 const PORT = process.env.PORT || 8080;
 const db = require("./models");
 
 // Creating express app and configuring middleware needed for authentication
+
+
 const app = express();
 app.use(express.urlencoded({ extended: true }));
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.static("public"));
 // We need to use sessions to keep track of our user's login status
