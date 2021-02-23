f7043ef6b93709f2c184875379f35127fb082787
desta mulu <destish21@yahoo.com>
2021-02-23 14:26:57 -0600

    added questions and sytles added one div form

--- public/js/questions.js
+++ public/js/questions.js
@@ -1,10 +1,33 @@
 $(document).ready(() => {
+  var answer;
   $("#single").on("click", () => {
     const callUrl3 = "https://jservice.io/api/random"
     $.ajax({
       url: callUrl3
     }).then(data => {
       console.log(data);
+      answer = (data[0].answer);
+      $("#displayQ").text(data[0].question);
+    });
+
+    $("#subAns").on("click", () => {
+      var myAnswer = $("#answer")
+      var myAnswerVal = (myAnswer.val().trim())
+
+        if (answer === myAnswerVal) {
+          alert("Correct " + answer + " is equal to " + myAnswerVal);
+         var xyz =  $("<div>").text("Correct " + answer + " is equal to " + myAnswerVal);
+         xyz.attr("class", "ansAlert1")
+         $(".ansAlert").append(xyz)
+        } else {
+          alert("Incorrect " + answer + " is not equal to " + myAnswerVal);
+
+          var abc =  $("<div>").text("Correct " + answer + " is not equal to " + myAnswerVal);
+          abc.attr("class", "ansAlert1")
+          $(".ansAlert").append(abc);
+        };
+      myAnswer.val("");
+
     });
   });
 
@@ -14,6 +37,34 @@ $(document).ready(() => {
       url: callUrl
     }).then(data => {
       console.log(data);
+      answer = (data[0].answer);
+      var myQuestions = []
+      for ( i = 0; i< data.length; i++){
+       $("#displayQ").text(data[i].question);
+        $("<div>").attr("class", "card").append($("<h5>"));
+        // var question = $("<h5>").addClass("card-text").text(data[i].question);
+      }
+      $("#subAns").on("click", () => {
+        var myAnswer = $("#answer")
+        var myAnswerVal = (myAnswer.val().trim())
+  
+          if (answer === myAnswerVal) {
+            alert("Correct " + answer + " is equal to " + myAnswerVal);
+           var xyz =  $("<div>").text("Correct " + answer + " is equal to " + myAnswerVal);
+           xyz.attr("class", "ansAlert1")
+           $(".ansAlert").append(xyz)
+          } else {
+            alert("Incorrect " + answer + " is not equal to " + myAnswerVal);
+  
+            var abc =  $("<div>").text("Correct " + answer + " is not equal to " + myAnswerVal);
+            abc.attr("class", "ansAlert1")
+            $(".ansAlert").append(abc);
+          };
+        myAnswer.val("");
+  
+      });
+
+      console.log(myQuestions)
     });
   })
 
@@ -25,6 +76,7 @@ $(document).ready(() => {
       console.log(data);
     });
   })
+
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
 
