60ba800c9eb1df1ab6ed8e51e94a17158ba543d4
Geoffrey Zimmerman <geoffzimmerman23@gmail.com>
2021-02-27 16:25:39 -0600

    questions.js routes

--- public/js/questions.js
+++ public/js/questions.js
@@ -1,10 +1,10 @@
 $(document).ready(() => {
   $("#single").on("click", () => {
-    window.location.replace("http://localhost:8080/gameBoard?type=single");
+    window.location.replace("/gameBoard?type=single");
   })
 
   $("#double").on("click", () => {
-    window.location.replace("http://localhost:8080/gameBoard?type=double");
+    window.location.replace("/gameBoard?type=double");
   })
 });
 
