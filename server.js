5c274f31abe1dd34d251a1a42fe132cd76d8ddd8
Tracy Golden <tracy80s2003@gmail.com>
2021-02-27 07:45:13 -0600

    added styling to home.handlebars

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const session = require("express-session");
 // Requiring passport as we've configured it
 const passport = require("./config/passport");
-const exphbs = require('express-handlebars')
+const exphbs = require('express-handlebars');
 
 // Setting up port and requiring models for syncing
 const PORT = process.env.PORT || 8080;
@@ -11,12 +11,11 @@ const db = require("./models");
 
 // Creating express app and configuring middleware needed for authentication
 
-
 const app = express();
 
-
 app.use(express.urlencoded({ extended: true }));
-app.engine('handlebars', exphbs());
+// Set Handlebars as the default templating engine.
+app.engine('handlebars', exphbs({ defaultLayout: "main"}));
 app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.static("public"));
@@ -27,9 +26,11 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+
 // Requiring our routes
 require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
+app.get()
 
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync({force: true}).then(() => {
