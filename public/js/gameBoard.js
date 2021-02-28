7f6eb2a00dcd02b5d7fbd1ab5a1f6c2141cb8251
Geoffrey Zimmerman <geoffzimmerman23@gmail.com>
2021-02-28 17:59:09 -0600

    obtain boolean from answer comparison

--- public/js/gameBoard.js
+++ public/js/gameBoard.js
@@ -1,8 +1,22 @@
-$(".btn-danger").on("click", function() {
-    const userQuestion = $("#questionSubmit").val()
+$(".btn-danger").on("click", function () {
+    const form = $("#quesForm")
+    let userQuestion = $("#questionSubmit").val()
     console.log(userQuestion)
     const questId = +$(this).parents(".questionId").attr("id").split("-")[1]
-    $.get("/api/gameBoard", function() {
-        console.log($("id").find(questId))
-    } )
+    console.log(questId)
+    $.get("/api/gameBoard").then(data => {
+        console.log(data)
+        for (let c = 0; c < data.length; c++) {
+            let dataClues = data[c].clues
+            for (let q = 0; q < dataClues.length; q++) {
+                let ansId = dataClues[q].id
+                if (questId === ansId) {
+                    let gameQuestion = dataClues[q].answer
+                    console.log(gameQuestion)
+                    console.log(gameQuestion === userQuestion)
+                    form.trigger("reset")
+                }
+            }
+        }
+    }).catch(err => console.log(err))
 })
