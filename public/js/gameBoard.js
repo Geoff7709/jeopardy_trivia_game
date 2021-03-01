dba6566fe4e7838e24317dd489b3a25a20acf89a
David Sotomayor <david.elijah.sotomayor@gmail.com>
2021-03-01 17:02:25 -0600

    create category api route to check answer; create button post logic for submitted answer

--- public/js/gameBoard.js
+++ public/js/gameBoard.js
@@ -1,8 +1,21 @@
-$(".btn-danger").on("click", function() {
-    const userQuestion = $("#questionSubmit").val()
-    console.log(userQuestion)
-    const questId = +$(this).parents(".questionId").attr("id").split("-")[1]
-    $.get("/api/gameBoard", function() {
-        console.log($("id").find(questId))
-    } )
+$(".btn-danger").on("click", function (event) {
+    event.preventDefault();
+    const category_id = +$(this).parents(".questionId").attr("data-category-id");
+    const id = +$(this).parents(".questionId").attr("id").split("-")[1]
+    const answer = $("#questionSubmit").val()
+
+    $.ajax({
+        type: "POST",
+        url: '/api/category',
+        data: JSON.stringify({ category_id, id, answer }),
+        contentType: 'application/json'
+    }).then(response => {
+        if (response.checkAnswer) {
+            console.log(response.checkAnswer)
+        } else {
+            console.log(response.checkAnswer)
+        }
+    }).catch(console.log)
+
 })
+
