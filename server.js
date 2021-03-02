15ff708e19aa0267f57469e5585e70b713970bba
David Sotomayor <david.elijah.sotomayor@gmail.com>
2021-03-02 17:47:40 -0600

    lint server js file

--- server.js
+++ server.js
@@ -3,35 +3,28 @@ const express = require("express");
 const session = require("express-session");
 // Requiring passport as we've configured it
 const passport = require("./config/passport");
-
-const exphbs = require('express-handlebars')
-const uest = require('uest')
-
-
+const exphbs = require("express-handlebars")
+const uest = require("uest")
 
 // Setting up port and requiring models for syncing
 const PORT = process.env.PORT || 8080;
 const db = require("./models");
 
 // Creating express app and configuring middleware needed for authentication
-
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 // Set Handlebars as the default templating engine.
-app.engine('handlebars', exphbs({ defaultLayout: "main"}));
-app.set('view engine', 'handlebars');
+app.engine("handlebars", exphbs({ defaultLayout: "main"}));
+app.set("view engine", "handlebars");
 app.use(express.json());
 app.use(express.static("public"));
 // We need to use sessions to keep track of our user's login status
 app.use(
-  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
-
-  );
+  session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
   app.use(passport.initialize());
   app.use(passport.session());
-  app.use(uest())
-  
+  app.use(uest());
 
 // Requiring our routes
 require("./routes/html-routes.js")(app);
