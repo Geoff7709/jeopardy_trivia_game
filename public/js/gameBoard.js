205a04bdb98b8b033f48f878bc56c6f672e22dd0
Geoffrey Zimmerman <geoffzimmerman23@gmail.com>
2021-03-06 10:01:15 -0600

    fix double penalty on pass

--- public/js/gameBoard.js
+++ public/js/gameBoard.js
@@ -77,17 +77,6 @@ $(document).ready(function () {
         $(this).parents(".questionId").prev().children().children().attr({ "class": "disabled btn btn-secondary btn-primary btn-lg", "href": " " });
     })
 
-
-    $(".btnPass").on("click", function (event) {
-        event.preventDefault();
-        const getScore = +document.getElementById('currentScore').innerHTML;
-        const value = +$(this).parents(".questionId").attr("data-value");
-        const newScore = getScore - value;
-        document.getElementById("currentScore").innerHTML = newScore;
-        $(this).parent().siblings("div.modal-body").children("textarea").val("");
-        $(this).parents(".questionId").prev().children().children().attr({ "class": "disabled btn btn-secondary btn-primary btn-lg", "href": " " });
-    })
-
     $(".exit").on("click", function () {
         window.location.replace("/highScore");
     });
